57aace0a2fa25940e74720339f004e26194387cc
annrinTN <annrumsey@comcast.com>
2020-01-19 17:33:14 -0600

    q2wk1

---
+++ GIT/scripts/q2wk1.sql
@@ -0,0 +1,6 @@
+-- #2 Write a query to look at just the first 10 rows. 
+-- What company is associated with the job posting on the 10th row?
+-- A: ExxonMobile
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
