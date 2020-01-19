c6cf7862d558fe30f7f44ac19b710a4c4dfa7ba8
annrinTN <annrumsey@comcast.com>
2020-01-19 17:48:58 -0600

    q3wk1

---
+++ GIT/scripts/q3wk1.sql
@@ -0,0 +1,7 @@
+--#3 How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+--A: 21 in TN and 27 in TN KY combined
+SELECT DISTINCT COUNT (location) , location
+FROM data_analyst_jobs
+WHERE location IN ('TN', 'KY')
+GROUP BY location
+;
