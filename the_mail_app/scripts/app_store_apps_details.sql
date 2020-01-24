ff08f20b7c0fedfd0c4b706a9613b0030a4886e4
annrinTN <annrumsey@comcast.com>
2020-01-23 18:28:48 -0600

    commit before checkout master

--- the_mail_app/scripts/app_store_apps_details.sql
+++ the_mail_app/scripts/app_store_apps_details.sql
@@ -1,4 +1,8 @@
+-- initial details of the app store data from play store
 SELECT COUNT (*)
 FROM app_store_apps;
 SELECT *
-FROM app_store_apps
+FROM app_store_apps;
+-- distinct fields in play_store_apps table
+SELECT COUNT (DISTINCT name) AS app_name
+FROM app_store_apps;
